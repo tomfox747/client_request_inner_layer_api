4a5b3ce8e6cb08c46c0277668699fc94472608bf
tomfox747 <thomasfox747@gmail.com>
2021-01-02 09:55:56 +0000

    added custom error handler

--- routes/data_router.js
+++ routes/data_router.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const data_service = require('../services/data_service');
-
+const customError = require('../utilities/errorHandler');
+const fileName = 'data_router';
 
 router.get('/get_data_by_building_code', async (req,res) =>{
-    const response = await data_service.get_data_by_building_code();
-    res.send(response);
+    try{
+        const response = await data_service.get_data_by_building_code();
+        res.send(response);
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_building_code'); 
+        return err;
+    }
 })
 
 router.get('/get_data_by_room_code', async (req,res) =>{
-    const response = await data_service.get_data_by_room_code();
-    res.send(response);
+    try{
+        const response = await data_service.get_data_by_room_code();
+        res.send(response);
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_room_code'); 
+        return err;
+    }
 })
 
 router.get('/get_data_by_sensor_code', async (req,res) =>{
-    const response = await data_service.get_data_by_sensor_code();
-    res.send(response);
+    try{
+        const response = await data_service.get_data_by_sensor_code();
+        res.send(response);
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_sensor_code'); 
+        return err;
+    }
 })
 
 module.exports = router;
--- services/data_service.js
+++ services/data_service.js
@@ -1,13 +1,31 @@
+const customError = require('../utilities/errorHandler');
+const fileName = 'data_service';
+
 const Get_data_by_building_code = () =>{
-    return 'showing data by building code'
+    try{
+        return 'showing data by building code'
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_building_code'); 
+        return err;
+    }
 }
 
 const Get_data_by_room_code = () =>{
-    return 'showing data by room code';
+    try{
+        return 'showing data by room code'
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_room_code'); 
+        return err;
+    }
 }
 
 const Get_data_by_sensor_code = () =>{
-    return 'showing data by sensor code';
+    try{
+        return 'showing data by sensor code'
+    }catch(e){
+        const err = new customError(e, e.code, fileName, 'get_data_by_sensor_code'); 
+        return err;
+    }
 }
 
 module.exports = {
